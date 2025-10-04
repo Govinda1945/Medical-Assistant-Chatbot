ba65dd3b04c18de26e8e14effa1d3de928f0e476
HemantChashtave26 <hemantchashtave@gmail.com>
2025-10-05 00:31:27 +0530

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    authonr = 'Govinda Chastwe',
+    author_email = 'govindachastwe54@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
